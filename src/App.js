dd1c8a174508e6af8f5f926c204ab444b3819847
Ranjith-M7 <ranjith.mathiyazhagan@gmail.com>
2024-11-14 23:48:51 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -5,15 +5,31 @@ import logo from "./assets/shared/logo.svg";
 function App() {
   return (
     <>
-      <header>
-        <nav>
-          <a href="l">
+      <header className="grid-main">
+        <nav className="grid-content topnav">
+          <a href="1">
             <img src={logo} height={40} width={40} alt="Typemaster home" />
           </a>
           <button className="button blue">Pre-order now</button>
         </nav>
       </header>
-      <main></main>
+      <main>
+        <section className="grid-main hero">
+          <div className="grid-content">
+            <div className="hero-text">
+              <h1>Typemaster Keyboard</h1>
+              <p>
+                Improve your productivity and gaming without breaking the bank.
+                Upgrade to a high quality mechanical typing experience.
+              </p>
+              <div className="">
+                <button className="button orange">Pre-order now</button>
+                <p>Release on 5/27</p>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
       <footer></footer>
     </>
   );
